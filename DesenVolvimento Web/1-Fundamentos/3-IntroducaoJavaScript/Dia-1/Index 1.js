b3a7704ab52f284ca29520c455d23063ba680ac4
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-06 11:55:33 -0300

    exercicio 8

--- DesenVolvimento Web/1-Fundamentos/3-IntroducaoJavaScript/Dia-1/Index 1.js
+++ DesenVolvimento Web/1-Fundamentos/3-IntroducaoJavaScript/Dia-1/Index 1.js
@@ -42,3 +42,15 @@ case 'cavalo':
 default:
     console.log('Escolha uma peça válida!');
 };
+
+
+let num1 = 11;
+let num2 = 19;
+let num3 = 31;
+
+
+if (num1 %2 ===0 || num2 %2 ===0|| num3 %2 ===0){
+    console.log(true)
+}else{
+    console.log(false)
+}
