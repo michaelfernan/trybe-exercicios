7671609f385e6e14ab82e4bcb1258e47b5008a84
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-06 11:51:00 -0300

    exercicio 5

--- DesenVolvimento Web/1-Fundamentos/3-IntroducaoJavaScript/Dia-1/Index 1.js
+++ DesenVolvimento Web/1-Fundamentos/3-IntroducaoJavaScript/Dia-1/Index 1.js
@@ -14,3 +14,31 @@ if( angulo1 > 0 && angulo2 >0 &&  angulo3> 0){
         console.log(false)
     }
 }else{console.loge("Angulo invalido")}
+
+
+
+
+let peca = 'Peão';
+
+switch (peca.toLowerCase()) {
+case 'torre':
+    console.log('Torre: Se movimenta em Horizontal e vertical.');
+    break;
+case 'bispo':
+    console.log('Bispo: Se movimenta em  diagonais.');
+    break;
+case 'rei':
+    console.log('Rei: Se movimenta em ma casa para qualquer direção.');
+    break;
+case 'rainha':
+    console.log('Rainha:  Se movimenta em diagonal, horizontal e vertical.');
+    break;
+case 'peão':
+    console.log("Peão:  Movimenta em uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+case 'cavalo':
+    console.log('Cavalo:  Se movimenta  em "L" pode pular sobre as peças.');
+    break;
+default:
+    console.log('Escolha uma peça válida!');
+};
