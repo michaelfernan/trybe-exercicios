b758ba2a66b3dcabf110f40f982c98cd706a222a
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-06 11:31:18 -0300

    exercicioa 1 a 3

---
+++ DesenVolvimento Web/1-Fundamentos/3-IntroducaoJavaScript/Dia-1/Index.js
@@ -0,0 +1,14 @@
+num1 = 10;
+num2 = 19;
+num3 = 31;
+
+
+if (num1 > num2 && num1 > num3){
+    console.log(num1)
+}  else if (num2 > num1 && num2> num3){
+    console.log(num2)
+}
+else{
+    console.log(num3)
+}
+
